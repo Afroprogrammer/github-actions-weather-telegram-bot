cb1bef1e560cea0955f1e385cb9257e1ace0d949
Franklin <forsonfranklin@gmail.com>
2020-11-19 10:44:41 +0000

    created the app

---
+++ index.js
@@ -0,0 +1,33 @@
+require('dotenv').config()
+const fetch = require('node-fetch')
+const Telegram = require('node-telegram-bot-api')
+const bot = new Telegram(process.env.TELEGRAM_TOKEN)
+
+
+const weatherToken = process.env.WEATHER_API_TOKEN
+const weatherURL = new URL('https://api.openweathermap.org/data/2.5/weather')
+weatherURL.searchParams.set('lat', '5.6242019')
+weatherURL.searchParams.set('lon', '-0.2276807')     
+weatherURL.searchParams.set('APPID', weatherToken)
+weatherURL.searchParams.set('units', 'imperial')
+
+const getWeatherData = async () =>{
+    const resp  = await fetch(weatherURL.toString())
+    const body = await resp.json()
+    return body 
+}
+
+const generateWeatherMessage = weatherData =>
+`The weather in ${weatherData.name}: ${weatherData.weather[0].description}. Current temperature is 
+${weatherData.main.temp},
+ with a low temp of ${weatherData.main.temp_min} and high of ${weatherData.main.temp_max}.`
+
+  
+ const main = async () => {
+    const weatherData = await getWeatherData()
+    const weatherString = generateWeatherMessage(weatherData)
+    // console.log(weatherString)
+    bot.sendMessage(process.env.TELEGRAM_CHAT_ID, weatherString)
+  } 
+
+  main()
